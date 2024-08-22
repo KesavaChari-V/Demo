85331bf5799b6b775f26453aa873faeb0002c230
AdmiN <AdmiN@ITWS0044>
2024-08-22 11:15:27 +0530

    last

--- Sample/src/com/lbrce/sample/Sample.java
+++ Sample/src/com/lbrce/sample/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
-		System.out.println("Chari");
+		System.out.println("Chari mg");
 		System.out.println("Third Line");
 		System.out.println("Chari Bot");
 	}
