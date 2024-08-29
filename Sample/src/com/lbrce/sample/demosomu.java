1a7415ef4d79a5a5fdd3058d955e48e0fe045ef1
AdmiN <AdmiN@ITWS065>
2024-08-29 10:24:44 +0530

    Demo somu is created

---
+++ Sample/src/com/lbrce/sample/demosomu.java
@@ -0,0 +1,10 @@
+package com.lbrce.sample;
+
+public class demosomu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This project is created by Somu");
+	}
+
+}
